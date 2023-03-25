9455e06b962c93ba31fb8ae36a6951723cd3cf31
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-03-24 19:03:00 -0600

    avance

--- Retrofit/app/src/main/java/com/example/retrofit/MainActivity.kt
+++ Retrofit/app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.retrofit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.retrofit.models.ImageRandom
 import com.example.retrofit.network.API
@@ -16,6 +18,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         traerImagenAleatoria()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_images, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.option_menu_list_images){
+            Toast.makeText(this, "Option Menu 1", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun traerImagenAleatoria(){
