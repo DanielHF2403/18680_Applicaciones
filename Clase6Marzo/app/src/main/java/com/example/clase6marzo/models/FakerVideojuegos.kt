5310539e69140ef82198ed08a437a4efa13a857f
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-04-16 22:28:00 -0600

    Avance

--- Clase6Marzo/app/src/main/java/com/example/clase6marzo/models/FakerVideojuegos.kt
+++ Clase6Marzo/app/src/main/java/com/example/clase6marzo/models/FakerVideojuegos.kt
@@ -8,14 +8,14 @@ class FakerVideojuegos {
         var videogames : ArrayList<Videojuego>
         videogames = arrayListOf<Videojuego>()
 
-        videogames.add(Videojuego(1, "Pokemon Violet", 1300F, "Nintendo Switch", R.drawable.poke))
-        videogames.add(Videojuego(2, "Resident Evil 5", 500F, "Xbox 360", R.drawable.resident5))
-        videogames.add(Videojuego(3, "Scarlet Nexus", 900F, "Xbox One", R.drawable.scarletnexus))
-        videogames.add(Videojuego(4, "The Last of Us", 900F, "PS4", R.drawable.thelastofus))
-        videogames.add(Videojuego(5, "Borderlands 2", 400F, "PC", R.drawable.borderlands))
-        videogames.add(Videojuego(6, "Code Vein", 1000F, "Xbox One", R.drawable.codevein))
-        videogames.add(Videojuego(7, "Gears Of War 3", 300F, "Xbox 360", R.drawable.gears3))
-        videogames.add(Videojuego(8, "Halo Reach", 300F, "Xbox 360", R.drawable.haloreach))
+        videogames.add(Videojuego(1, "Pokemon Violet", 1300F, "Nintendo Switch", R.drawable.poke, "Everyone"))
+        videogames.add(Videojuego(2, "Resident Evil 5", 500F, "Xbox 360", R.drawable.resident5, "Mature"))
+        videogames.add(Videojuego(3, "Scarlet Nexus", 900F, "Xbox One", R.drawable.scarletnexus, "Teen"))
+        videogames.add(Videojuego(4, "The Last of Us", 900F, "PS4", R.drawable.thelastofus, "Mature"))
+        videogames.add(Videojuego(5, "Borderlands 2", 400F, "PC", R.drawable.borderlands, "Mature"))
+        videogames.add(Videojuego(6, "Code Vein", 1000F, "Xbox One", R.drawable.codevein, "Mature"))
+        videogames.add(Videojuego(7, "Gears Of War 3", 300F, "Xbox 360", R.drawable.gears3,"mature"))
+        videogames.add(Videojuego(8, "Halo Reach", 300F, "Xbox 360", R.drawable.haloreach, "Mature"))
         return videogames
     }
 
