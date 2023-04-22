c57f340ea77ce5f7a9b5c772c038e232658c6249
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-04-21 19:46:45 -0500

    examen

--- Parcial2/app/src/main/java/edu/iest/parcial2/MainActivity.kt
+++ Parcial2/app/src/main/java/edu/iest/parcial2/MainActivity.kt
@@ -1,11 +1,39 @@
 package edu.iest.parcial2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var ivcerrar = findViewById<ImageView>(R.id.ivCerrar)
+        var tvcerrar = findViewById<TextView>(R.id.tvCerrar)
+        var tvperfil = findViewById<TextView>(R.id.tvProfile)
+        var ivperfil = findViewById<ImageView>(R.id.ivProfile)
+
+        ivcerrar.setOnClickListener {
+            finish()
+        }
+
+        tvcerrar.setOnClickListener {
+            finish()
+        }
+
+        ivperfil.setOnClickListener {
+            val s = Intent(this, SolicitarPreferencias::class.java)
+            startActivity(s)
+            finish()
+        }
+
+        tvperfil.setOnClickListener {
+            val s = Intent(this, SolicitarPreferencias::class.java)
+            startActivity(s)
+            finish()
+        }
     }
 }
